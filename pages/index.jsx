23c7897c04d0e47de1b47723e4536a92efbbd478
Kai-Itakura <itakai199969@gmail.com>
2023-02-13 21:57:58 +0900

    ✨Reactを使った ページ制作の導入を学習

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>CUBE</h1>
+const Home = () => {
+  return (
+    <>
+      <header>HEADER</header>
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+
+      <footer>FOOTER</footer>
+    </>
+  );
 }
+
+export default Home;
