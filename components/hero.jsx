cedff354bed2732abc4ec9ce720afbfd876fb0a7
Kai-Itakura <itakai199969@gmail.com>
2023-02-15 20:26:22 +0900

    ✨ Sassを導入

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,4 +1,4 @@
-import classes from 'styles/hero.module.css'
+import classes from 'styles/hero.module.scss'
 
 const Hero = ({ title, subtitle, imageOn = false }) => {
     return (
--- components/logo.jsx
+++ components/logo.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import classes from 'styles/logo.module.css';
+import classes from 'styles/logo.module.scss';
 
 const Logo = ({ boxOn = false }) => {
     return (
