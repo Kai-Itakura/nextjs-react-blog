c6ebd0c7d023c28b2206f0ba418639d140a45eda
Kai-Itakura <itakai199969@gmail.com>
2023-02-16 16:16:18 +0900

    💄 共通のスタイルでヘッダーのレイアウトを整えた

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,14 @@
 import Logo from "./logo";
 import Nav from "./nav";
+import classes from "styles/header.module.scss";
 
 const Header = () => {
     return (
         <header>
-            <Logo boxOn />
-            <Nav />
+            <div className={classes.flexContainer}>
+                <Logo boxOn />
+                <Nav />
+            </div>
         </header>
     )
 }
