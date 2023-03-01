a80367760b762c1cee895bf62b52a757cc8b1f11
Kai-Itakura <itakai199969@gmail.com>
2023-03-01 17:42:20 +0900

    ✨ Font Awesome を使ってContact とFooter にアイコンを追加

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,9 +1,11 @@
 import classes from 'styles/contact.module.scss';
+import Social from './social';
 
 const Contact = () => {
     return (
         <div className={classes.stack}>
             <h3 className={classes.heading}>Contact</h3>
+            <Social />
             <address>cube@web.mail.address</address>
         </div>
     );
--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,7 @@
 import Logo from "./logo";
 import classes from 'styles/footer.module.scss';
 import Container from "./container";
+import Social from "./social";
 
 const Footer = () => {
     return (
@@ -8,7 +9,7 @@ const Footer = () => {
             <Container>
                 <div className={classes.flexContainer}>
                     <Logo />
-                    [ソーシャル]
+                    <Social />
                 </div>
             </Container>
         </footer>
