3181a994075a19ab2d652b113feac7c514c31400
Kai-Itakura <itakai199969@gmail.com>
2023-03-01 15:54:55 +0900

    next.jsでFont Awesome を使う設定を記述

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,10 @@
 import Layout from '@/components/Layout';
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+
+// Font Awesome の設定
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false
 
 export const App = ({ Component, pageProps }) => {
   return (
