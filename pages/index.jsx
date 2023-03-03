e5921be1cb1613384116c8617e9e0fa25bd307d6
Kai-Itakura <itakai199969@gmail.com>
2023-03-04 00:49:30 +0900

    トップページに記事一覧を表示

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
 import Container from "@/components/container";
+import Posts from "@/components/posts";
+import { getAllPosts } from "@/lib/api";
+import { eyecatchLocal } from "@/lib/constants";
 import Hero from "components/hero";
 import Meta from 'components/meta';
+import { getPlaiceholder } from "plaiceholder";
+import Pagination from "@/components/pagination";
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <Container>
       <Meta />
@@ -11,8 +16,28 @@ const Home = () => {
         subtitle='アウトプットしていくサイト'
         imageOn
       />
+      <Posts posts={posts} />
+      <Pagination nextUrl="/blog" nextText="More Posts" />
     </Container>
   )
+}
+
+export const getStaticProps = async () => {
+  const posts = await getAllPosts(4);
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal;
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url);
+    post.eyecatch.blurDataURL = base64;
+  }
+
+  return {
+    props: {
+      posts: posts,
+    }
+  }
 }
 
 export default Home;
