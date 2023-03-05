a0429ff42c2c75554cccfe3d0600b7ade92f3fa6
Kai-Itakura <itakai199969@gmail.com>
2023-03-05 23:43:18 +0900

    スマートフォン画面でのハンバーガーメニューを作成

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,22 +1,48 @@
 import Link from "next/link";
-import classes from 'styles/nav.module.css';
+import { useState } from "react";
+import classes from 'styles/nav.module.scss';
 
 const Nav = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen((prev) => !prev)
+    }
+
+    const closeNav = () => {
+        setIsNavOpen(false)
+    }
+
     return (
-        <nav>
+        <nav className={isNavOpen ? classes.open : classes.close}>
+            {isNavOpen && (
+                <style jsx global>{`
+                    @media(max-width: 767px) {
+                        body {
+                            overflow: hidden;
+                            position: fixed;
+                            width: 100%;
+                        }
+                    }
+                `}</style>
+            )}
+            <button className={classes.btn} onClick={toggleNav}>
+                <span className={classes.bar}></span>
+                <span className='sr-only'>MENU</span>
+            </button>
             <ul className={classes.list}>
                 <li>
-                    <Link href='/'>
+                    <Link onClick={closeNav} href='/'>
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link href='/about'>
+                    <Link onClick={closeNav} href='/about'>
                         About
                     </Link>
                 </li>
                 <li>
-                    <Link href='/blog'>
+                    <Link onClick={closeNav} href='/blog'>
                         Blog
                     </Link>
                 </li>
