0558e73aa28cfa7e6dabf82cb4042cd99e9725e6
Kai-Itakura <itakai199969@gmail.com>
2023-03-06 13:13:41 +0900

    アコーディオンの開閉アニメーションを作成

--- components/accordion.jsx
+++ components/accordion.jsx
@@ -1,6 +1,6 @@
 import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import classes from 'styles/accordion.module.scss';
 
 const Accordion = ({ heading, children }) => {
@@ -10,6 +10,8 @@ const Accordion = ({ heading, children }) => {
         setTextIsOpen((prev) => !prev);
     }
 
+    const refText = useRef(null);
+
     return (
         <div className={textIsOpen ? classes.open : classes.close}>
             <h3 className={classes.heading}>
@@ -18,7 +20,15 @@ const Accordion = ({ heading, children }) => {
                     <FontAwesomeIcon icon={faCircleChevronDown} className={classes.icon} />
                 </button>
             </h3>
-            <div className={classes.text}>
+            <div
+                className={classes.text}
+                ref={refText}
+                style={{
+                    '--text-height': refText.current
+                        ? `${refText.current.scrollHeight}px`
+                        : '0',
+                }}
+            >
                 <div className={classes.textInner}>{children}</div>
             </div>
         </div>
